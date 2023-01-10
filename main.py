fef10ce18801a19c559b59c2aeafff496fa86aae
Zero <80227201+AI-Zero-One@users.noreply.github.com>
2023-01-10 02:50:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+
+print("=======================================")
+time.sleep(0.75)
+print("     00000000P0     (=========)")  
+time.sleep(0.75)
+print("     00    0P0          00     ")
+time.sleep(0.75)
+print("     00    P00          00     ")
+time.sleep(0.75)
+print("     00000000           00     ") 
+time.sleep(0.75)
+print("     00                 00     ")
+time.sleep(0.75)
+print("     00                 00     ")
+time.sleep(0.75)
+print("     00             (=========)   OS")
+time.sleep(0.75)
+print("=======================================")
+
+
+ 
